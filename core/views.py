036b767fded6cd548f737b039376061404231000
Hemang Patel <136294289+Hemang-devlops@users.noreply.github.com>
2023-09-28 19:22:10 +0530

    added req file

--- core/views.py
+++ core/views.py
@@ -30,31 +30,3 @@ def contact(request):
         'form': fm
     }
     return render(request, 'core/contact.html', context)
-
-
-# def saveCustomerData(request):
-    if request.method == 'POST':
-        # name = request.POST.get('inputName')
-        # email = request.POST.get('inputEmail')
-        # subject = request.POST.get('inputSubject')
-        # message = request.POST.get('inputTextarea')
-        # data = customerData(name=name, email=email,
-        #                     subject=subject, message=message)
-        # data.save()
-        fm = InquiryForm(request.POST)
-        # Using forms.py
-        if fm.is_valid():
-            name = fm.cleaned_data['inputName']
-            email = fm.cleaned_data['inputEmail']
-            subject = fm.cleaned_data['inputSubject']
-            message = fm.cleaned_data['inputTextarea']
-            data = customerData(name=name, email=email,
-                                subject=subject, message=message)
-            data.save()
-    else:
-        fm = InquiryForm()
-    context = {
-        'contact': 'active',
-        'form': fm
-    }
-    return render(request, 'core/contact.html', context)
